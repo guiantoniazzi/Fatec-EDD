7c00c9145fa1ac5baaf47b525ebf2133112dedd5
Leticia Candido <leticiacandidoarq@gmail.com>
2024-06-02 23:20:36 -0300

    ajuste leitura arquivo|adicao read.me|alteracao arquivo saida

--- Projeto_N2 (EDD).cpp
+++ Projeto_N2 (EDD).cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
+#include <string>
 #include <locale.h>
 #include <time.h>
 #include <sys/types.h>
 #include <dirent.h>
 #include <conio.h>
-#include <fstream> 
+#include <fstream>
+#include <fileapi.h>
 
 /*
 [IDEIA]
@@ -18,12 +20,12 @@
 	- INICIA O TEMPO 1
 	- APLICA A ORGANIZAÇÃO A
 	- TERMINA O TEMPO 1
-	
+
 - DUPLICA O VETOR BASE PARA OUTRO VETOR B - Quick Sort
 	- INICIA O TEMPO 2
 	- APLICA A ORGANIZAÇÃO B
 	- TERMINA O TEMPO 2
-	
+
 - DUPLICA O VETOR BASE PARA OUTRO VETOR C - Mergesort
 	- INICIA O TEMPO 3
 	- APLICA A ORGANIZAÇÃO C
@@ -31,6 +33,19 @@
 
 - COMPARA AS 3 VERSÕES E QUAL TEVE O MELHOR DESEMPENHO EM TEMPO
 */
+
+const std::string computador = "Leticia";
+
+struct linha{
+        std::string nomeArquivo;
+        float tempoSelection;
+        float tempoQuick;
+        float tempoMerge;
+        std::string tamanho;
+        std::string duplicidade;
+        std::string org_previa;
+        std::string caminho;
+};
 
 //Funções
 float delta_tempo(clock_t Tempo_Final, clock_t Tempo_Inicial)
@@ -40,8 +55,8 @@ float delta_tempo(clock_t Tempo_Final, clock_t Tempo_Inicial)
 	resultado = resultado/1000;
 	return resultado;
 }
-  
-//Sort A  
+
+//Sort A
 void selection_sort (int vetor[], int tamanho)
 {
     int i, j, min, x;
@@ -63,24 +78,24 @@ void selection_sort (int vetor[], int tamanho)
 
 //Sort B
 void quick_sort(int vetor[], int esq, int dir){
-    int pivo = esq, i,ch,j;         
-    for(i=esq+1;i<=dir;i++){        
-        j = i;                      
-        if(vetor[j] < vetor[pivo]){     
-            ch = vetor[j];               
-            while(j > pivo){           
-                vetor[j] = vetor[j-1];      
-                j--;                    
+    int pivo = esq, i,ch,j;
+    for(i=esq+1;i<=dir;i++){
+        j = i;
+        if(vetor[j] < vetor[pivo]){
+            ch = vetor[j];
+            while(j > pivo){
+                vetor[j] = vetor[j-1];
+                j--;
             }
-            vetor[j] = ch;               
-            pivo++;                    
+            vetor[j] = ch;
+            pivo++;
         }
     }
-    if(pivo-1 >= esq){              
-        quick_sort(vetor,esq,pivo-1);      
+    if(pivo-1 >= esq){
+        quick_sort(vetor,esq,pivo-1);
     }
-    if(pivo+1 <= dir){              
-        quick_sort(vetor,pivo+1,dir);      
+    if(pivo+1 <= dir){
+        quick_sort(vetor,pivo+1,dir);
     }
  }
 
@@ -99,7 +114,7 @@ void mergeSort(int *vetor, int posicaoInicio, int posicaoFim) {
     vetorTemp = (int *) malloc(sizeof(int) * (posicaoFim - posicaoInicio + 1));
 
     while(i < metadeTamanho + 1 || j  < posicaoFim + 1) {
-        if (i == metadeTamanho + 1 ) { 
+        if (i == metadeTamanho + 1 ) {
             vetorTemp[k] = vetor[j];
             j++;
             k++;
@@ -131,112 +146,141 @@ void mergeSort(int *vetor, int posicaoInicio, int posicaoFim) {
     free(vetorTemp);
 }
 
+void escreveArquivo(linha registro){
+
+    DIR *ds;
+
+    ds = opendir("./saida");
+    if (ds == NULL) {
+        mkdir("./saida");
+        ds = opendir("./saida");
+    }
+
+    std::string caminhoResultado = "./saida/resultado.csv";
+//    std::ofstream outfile (caminhoResultado.c_str());
+    std::ofstream outfile (caminhoResultado.c_str(), std::ios::app);
+
+    outfile << registro.nomeArquivo << ";";
+    outfile << registro.tempoSelection << ";";
+    outfile << registro.tempoQuick << ";";
+    outfile << registro.tempoMerge << ";";
+    outfile << computador << ";";
+    outfile << registro.caminho << std::endl;
+
+    outfile.close();
+}
+
+void executaMetodos(const char* caminhoArquivo, std::string nomeArquivo){
+
+    FILE *arq;
+    char Linha[100000];
+    char *result;
+    int i = 0;
+    int minhaArray[99999];
+    int tamanho = 0;
+
+    arq = fopen(caminhoArquivo, "rb");
+    if (arq == NULL)
+    {
+        printf("Problemas na abertura do arquivo\n");
+    }
+    while (!feof(arq))
+    {
+        result = fgets(Linha, 100000, arq);
+        if (result) {
+            //std::cout << Linha << "\n";
+            minhaArray[i] = atoi(Linha);
+            i++;
+            tamanho = i;
+        }
+    }
+    fclose(arq);
+
+    //Variáveis de Tempo
+    clock_t tempoI, tempoF;
+    float tempoA, tempoB, tempoC;
+
+    //Inicio do TempoA
+    tempoI = clock();
+
+    //método A - Selection Sort
+    selection_sort(minhaArray, tamanho);
+
+    //Fim do TempoA
+    tempoF = clock();
+
+    //Cálculo de TempoA
+    tempoA = delta_tempo(tempoF, tempoI);
+
+    //Inicio do TempoB
+    tempoI = clock();
+
+    //método B - Quick Sort
+    quick_sort(minhaArray, 10, 20);
+
+    //Fim do TempoB
+    tempoF = clock();
+
+    //Cálculo de TempoB
+    tempoB = delta_tempo(tempoF, tempoI);
+
+    //Inicio do TempoC
+    tempoI = clock();
+
+    //método C - Mergesort
+    mergeSort(minhaArray, 0, tamanho - 1);
+
+    //Fim do TempoC
+    tempoF = clock();
+
+    //Cálculo de TempoC
+    tempoC = delta_tempo(tempoF, tempoI);
+
+
+    linha registro;
+
+    registro.nomeArquivo = nomeArquivo;
+    registro.tempoSelection = tempoA;
+    registro.tempoQuick = tempoB;
+    registro.tempoMerge = tempoC;
+    registro.caminho = caminhoArquivo;
+
+    escreveArquivo(registro);
+}
+
+
+void leDir(const char* nomeDir){
+
+    DIR* dirp;
+	struct dirent* entryp;
+	dirp = opendir(nomeDir);
+
+
+
+    while((entryp=readdir(dirp)) != nullptr){
+       std::string fileName = entryp->d_name;
+       std::string caminho = std::string(nomeDir) + "/" + fileName;
+
+       DWORD atributos = GetFileAttributes(caminho.c_str());
+       if (atributos & FILE_ATTRIBUTE_DIRECTORY){
+            if(fileName != "." && fileName != ".."){
+                leDir(caminho.c_str());
+            }
+       }
+       else if(atributos & FILE_ATTRIBUTE_ARCHIVE){
+           executaMetodos(caminho.c_str(), fileName);
+           std::cout << "Processando..." << fileName << "\n";
+       }
+    }
+
+    closedir(dirp);
+}
+
+
 int main()
 {
-	DIR *dp, *ds;
-	struct dirent *ep;
-	dp = opendir("./txtTeste");
-	
-	int minhaArray[99999];
-	int tamanho = 0;
-	
-	if(dp != NULL) {
-		while(ep = readdir(dp)) {
-			std::string nomeArquivo = ep->d_name;
-			if(nomeArquivo.find(".txt") != std::string::npos) {
-			
-				FILE *arq;
-			  	char Linha[100];
-			  	char *result;
-			  	int i = 0;
-			  	
-			  	std::string caminho = "./txtTeste/" + nomeArquivo;
-			
-			  	const char *cstr = caminho.c_str();
-			  	
-			  	arq = fopen(cstr, "rb");
-			  	if (arq == NULL)
-			  	{
-			    	printf("Problemas na abertura do arquivo\n");
-			  	}
-			  	while (!feof(arq))
-			  	{
-			      	result = fgets(Linha, 100, arq);
-			      	if (result) {
-			      		//std::cout << Linha << "\n";
-						minhaArray[i] = atoi(Linha);
-						i++;
-						tamanho = i;
-					}
-			  	}
-			  	fclose(arq);
-			  	
-			  	//Variáveis de Tempo
-			    clock_t tempoI, tempoF;
-				float tempoA, tempoB, tempoC;
-				
-				//Inicio do TempoA
-				tempoI = clock();
-				
-				//método A - Selection Sort
-				selection_sort(minhaArray, tamanho);
-				
-				//Fim do TempoA
-				tempoF = clock();
-				
-				//Cálculo de TempoA
-				tempoA = delta_tempo(tempoF, tempoI);
-				
-				//Inicio do TempoB
-				tempoI = clock();
-				
-				//método B - Quick Sort
-				quick_sort(minhaArray, 10, 20);
-				
-				//Fim do TempoB
-				tempoF = clock();
-				
-				//Cálculo de TempoB
-				tempoB = delta_tempo(tempoF, tempoI);
-				
-				//Inicio do TempoC
-				tempoI = clock();
-				
-				//método C - Mergesort
-				mergeSort(minhaArray, 0, tamanho - 1);
-			
-				//Fim do TempoC
-				tempoF = clock();
-				
-				//Cálculo de TempoC
-				tempoC = delta_tempo(tempoF, tempoI);
-			
-				//FINALIZAÇÃO
-				std::cout << nomeArquivo << "\n";
-				printf("%0.10f \n", tempoA);
-				printf("%0.10f \n", tempoB);
-				printf("%0.10f \n\n", tempoC);
-				
-				ds = opendir("./saida");
-				if (ds == NULL) {
-					mkdir("./saida");
-					ds = opendir("./saida");
-				}
-				
-				std::string caminha = "./saida/" + nomeArquivo;
-				std::ofstream outfile (caminha.c_str());
-
-				outfile << nomeArquivo << std::endl;
-				outfile << tempoA << std::endl;
-				outfile << tempoB << std::endl;
-				outfile << tempoC << std::endl;
-
-				outfile.close();
-			}
-		}
-		(void) closedir(dp);
-	}
-	else {
-		printf("Não foi possível abrir a pasta");
-	}
+    const char* diretorioOrigem = "./arquivos";
+    leDir(diretorioOrigem);
+    std::cout << "FIM";
+    return 0;
 }
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Fatec-EDD
+# Fatec-EDD ADSMA3
+
+Alunos:
+Guilherme Antoniazzi Pinelli  
+Letícia Cândido de Oliveira  
+Lucas Henrique do Nascimento de Paula  
+Marcello Castadelli  
+Victor Garcia Elaro
+
+
+## Sobre:
+Trabalho para a disciplina Estrutura de Dados da Fatec São Caetano do Sul. Foi realizada uma análise de performance para três algoritmos de métodos de ordenação: Selection sort, Quick sort e Merge sort.
+
+### Estrutura do Repositório:
+* **arquivos**: pasta onde estarão os arquivos de texto utilizados para os testes
+* **saida**: pasta onde estará o arquivo .csv com o resultado do processamento e análise posterior
+* **Projeto_N2 (EDD).cpp**: arquivo contendo o código utilizado
+
+## Observações importantes:
+1. Ao abrir o código, lembrar de renomear a constante `computador`, que deverá conter o nome de referência ao computador que executará o teste para posterior comparação;
+2. Não alterar o nome da pasta contendo os arquivos de texto para teste. Caso o faça, altere no código na variável `diretorioOrigem`, quando atribuída no `main()`;  
